a9daef695eee3dc946da0d9a9d82baec0e258e8f
Jeff Quinn <ns8riders@me.com>
2017-05-18 18:07:09 -0600

    page 80

---
+++ time_tracking_app/public/js/app.js
@@ -0,0 +1,94 @@
+class TimersDashboard extends React.Component {
+  render() {
+    return (
+      <div className='ui three column centered grid'>
+        <div className='column'>
+          <EditableTimerList />
+          <ToggleableTimerForm
+            isOpen={true}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+class EditableTimerList extends React.Component {
+  render() {
+    return(
+      <div id='timers'>
+        <EditableTimer
+          title='Learn React.js'
+          project='Build Beautiful Apps/Websites'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title='Create React Native Apps baby'
+          project='Make Something People Love!'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    );
+  }
+}
+
+class EditableTimer extends React.Component {
+  render() {
+    if (this.props.editFormOpen) {
+      return (
+        <TimerForm
+          title={this.props.title}
+          project={this.props.project}
+        />
+      );
+    } else {
+      return (
+        <Timer
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      );
+    }
+  }
+}
+
+class TimerForm extends React.Component {
+  render() {
+    const submitText = this.props.title ? 'Update' : 'Create';
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='ui form'>
+            <div className='field'>
+              <label>Title</label>
+              <input type='text' defaultValue={this.props.title} />
+            </div>
+            <div className='field'>
+              <label>Project</label>
+              <input type='text' defaultValue={this.props.project} />
+            </div>
+            <div className='ui two bottom attached buttons'>
+              <button className='ui basic blue button'>
+                {submitText}
+              </button>
+              <button className='ui basic red button'>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <TimersDashboard />,
+  document.getElementById('content')
+);
