705b8856acfc7c440480d20f0b496fb55abad735
Jeff Quinn <ns8riders@me.com>
2017-05-22 20:23:49 -0600

    page 137

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -40,6 +40,8 @@ class TimersDashboard extends React.Component {
     this.setState({
       timers: this.state.timers.concat(t),
     });
+
+    client.createTimer(t);
   };
 
   updateTimer = (attrs) => {
@@ -55,12 +57,18 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.updateTImer(attrs);
   };
 
   deleteTimer = (timerId) => {
     this.setState({
       timers: this.state.timers.filter(t => t.id !== timerId),
     });
+
+    client.deleteTimer(
+      { id: timerId }
+    );
   };
 
     startTimer = (timerId) => {
@@ -78,6 +86,10 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.startTimer(
+      { id: timerId, start: now }
+    );
   };
 
   stopTimer = (timerId) => {
@@ -97,6 +109,10 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.stopTimer(
+      { id: timerId, stop: now }
+    );
   };
 
   render() {
